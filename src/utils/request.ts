168b55b711610b5361360f6c943345f51581b90a
蔡逍侠 <362652565@qq.com>
2022-08-31 13:38:06 +0800

    feat: api 替换

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const req = axios.create({
-  baseURL: 'https://cxx.smilec.cc/v1',
+  baseURL: 'https://api.moono.cc/v1',
   // baseURL: 'http://localhost:3001/v1',
 })
 
