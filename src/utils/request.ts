5bf93d6d6d5cc3d4422d7a6b218922940d88b4fd
蔡逍侠 <362652565@qq.com>
2022-09-15 09:15:45 +0800

    feat: api 替换

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const req = axios.create({
-  baseURL: 'https://api.moono.cc/v1',
+  // baseURL: 'https://api.moono.cc/v1',
   // baseURL: 'http://localhost:3001/v1',
+  baseURL: 'http://121.5.196.193:3001/v1',
 })
 
 export default req
